dc517ae7496ceae59f8d84137758f28dd767e19c
Euab <31598728+Euab@users.noreply.github.com>
2017-10-22 00:30:41 +0100

    beginning messages

--- app/app.py
+++ app/app.py
@@ -3,13 +3,18 @@ from flask_socketio import SocketIO, emit
 
 app = Flask(__name__)
 
-app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
+app.config['SECRET_KEY'] = 'dj3289fnij32908uhjcn329'
 app.debug = True
 socketio = SocketIO(app)
 
 @app.route('/')
 def index():
-    return 'hello world'
+    return render_template('chat.html')
+
+@socketio.on('emit event')
+def emit_handler(json):
+    print("MSG: " + str(json))
+    socketio.emit('resp', json)
 
 if __name__ == '__main__':
     socketio.run(app)
