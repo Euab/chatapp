d3a9d000a137bc5bfefd9708101ee90eccfdb787
Euab <euanmills26@gmail.com>
2017-10-26 17:16:32 +0100

    Improved electron

--- app/app.js
+++ app/app.js
@@ -1,23 +1,62 @@
-const electron = require('electron');
-const path  = require('path');
-const url = require('url');
+const electron = require("electron")
+const log = require("electron-log")
+const ipcMain = electron.ipcMain
 
-const {app, BrowserWindow} = electron;
+const app = electron.app
+const BrowserWindow = electron.BrowserWindow
 
 let mainWindow;
 
-// Wait and listen for the app to be ready
-app.on('ready', function() {
-  // Create a new Chromium window
-  mainWindow = new BrowserWindow({});
-  // Loading HTML into the window
+const path = require('path')
+const url = require('url')
+let autoUpdater = undefined
+
+log.info('Electron started...')
+log.info('Version: ' + app.getVersion())
+log.transport.file.level = 'info'
+
+const buildDestination = "./ui";
+
+function isDebug() {
+  return process.argv.length > 3 && process.argv[2] === 'debug'
+}
+
+function createWindow() {
+  mainWindow = new BrowserWindow({
+    width: 1000,
+    height: 800
+  })
+
+  mainWindow.webContents.on('did-finish-load', () => {
+    mainWindow.setTitle('ChatApp (' + app.getVersion() + ')')
+  })
+
+  global.dirname = __dirname;
+  global.userDataPath = app.getPath('userData')
+
   mainWindow.loadURL(url.format({
-    pathname: path.join(__dirname, 'index.html'),
+    pathname: path.join(__dirname, buildDestination + '/index.html'),
     protocol: 'file:',
     slashes: true
-  }));
-  // Quit app when closed
+  }))
+
   mainWindow.on('closed', function() {
-    app.quit();
-  });
-});
+    mainWindow = null
+  })
+}
+
+app.on('ready', function() {
+  createWindow()
+})
+
+app.on('window-all-closed', function() {
+  if(process.platform !== 'darwin') {
+    app.quit()
+  }
+})
+
+app.on('activate', function() {
+  if(mainWindow === null) {
+    createWindow()
+  }
+})
