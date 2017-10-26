97173b816986644543590a9609c8b9a0db43cf3b
Tonk1e <ttt0nk1e@gmail.com>
2017-10-26 23:45:11 +0100

    fixed stuff Please enter the commit message for your changes. Lines starting

--- website/app.py
+++ website/app.py
@@ -1,53 +1,3 @@
-<<<<<<< HEAD
-from flask import Flask, render_template, request, redirect, url_for
-import os
-import redis
-
-app = Flask(__name__)
-
-db = redis.Redis.from_url("redis://localhost", decode_responses=True)
-
-app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
-app.debug = True
-
-@app.route('/')
-def index():
-	return render_template('index.html')
-
-@app.route('/about')
-def about():
-	return render_template('about.html')
-
-@app.route('/chat')
-def chat():
-    return render_template('chat.html')
-
-@app.route('/login')
-def login():
-	return render_template('login.html')
-  
-def messageReceived():
-  print( 'message was received!!!' )
-
-# Handling HTTP errors
-@app.errorhandler(404)
-def not_found(e):
-    return render_template('404.html')
-
-@app.route('/#', methods=['POST', 'GET'])
-def email_submit():
-  email = request.form.get('email_submit')
-  db.append('user_email_list', email)
-  emaillist = db.get('user_email_list')
-  with open('./test.txt', 'w') as test:
-    print(emaillist, file=test)
-  return redirect(url_for('index'))
-
-
-if __name__ == '__main__':
-  app.run()
-  db.set('user_email_list', '')
-=======
 import os
 import redis
 import pymongo
@@ -176,4 +126,4 @@ def email_submit():
 if __name__ == '__main__':
   app.run()
   db.set('user_email_list', '')
->>>>>>> 96267f7f5754e634862078fc11a205df0d34eeac
+
